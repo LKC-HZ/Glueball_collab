04a06ad546344ed49e84454c30cc9424e8052303
LKC-HZ <235432438+LKC-HZ@users.noreply.github.com>
2026-08-30 16:58:40 +0800

    /

--- src/color_space/sc.py
+++
@@ -1,4 +0,0 @@
-import color_wf_py
-
-r = color_wf_py.run_color_wf_py(3,0,1)
-print(r)
--- src/color_space/test_sparse_5g.py
+++
@@ -1,8 +0,0 @@
-from color_wf_py_sparse import run_color_wf_py
-import time
-
-t0 = time.perf_counter()
-out = run_color_wf_py(0, 0, 5)
-t1 = time.perf_counter()
-print("Done in %.2f s" % (t1 - t0))
-print(out[:1000])
